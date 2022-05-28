30dbbde1dd4ffa4ad9cf3afad5f95adfb190a96c
hengoye <hengoye@sina.cn>
2022-05-28 14:19:18 +0800

    m

--- gost.sh
+++ gost.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt-get update
 wget "https://github.com/ginuerzh/gost/releases/download/v2.8.1/gost_2.8.1_linux_amd64.tar.gz"
 tar -zxvf gost_2.8.1_linux_amd64.tar.gz
-mv gost_2.8.1_linux_amd64/gost /usr/bin/gost
+sudo mv gost_2.8.1_linux_amd64/gost /usr/bin/gost
 sudo chmod +x /usr/bin/gost
 sudo wget https://raw.githubusercontent.com//henglxm/xmr/main/gost.service -O /etc/systemd/system/gost.service
 sudo systemctl daemon-reload
