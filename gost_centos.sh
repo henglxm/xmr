a4d500c51dbaab29f1a79dc12dbf01e0c067e158
hengoye <hengoye@sina.cn>
2022-06-01 02:36:01 +0800

    m

--- gost_centos.sh
+++ gost_centos.sh
@@ -1,4 +1,3 @@
-sudo yum install -y wget
 wget "https://github.com/ginuerzh/gost/releases/download/v2.8.1/gost_2.8.1_linux_amd64.tar.gz"
 tar -zxvf gost_2.8.1_linux_amd64.tar.gz
 sudo mv gost_2.8.1_linux_amd64/gost /usr/bin/gost
