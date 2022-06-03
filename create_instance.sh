e4bea5ed1839955a03d4a97031abd50c048c9058
hengoye <hengoye@sina.cn>
2022-06-03 21:19:58 +0800

    m

---
+++ create_instance.sh
@@ -0,0 +1,3 @@
+aws lightsail create-instances --instance-names {"Ubuntu-1","Ubuntu-2"} --availability-zone ap-southeast-1b --region ap-southeast-1 --blueprint-id ubuntu_20_04 --bundle-id nano_2_0 --profile
+aws lightsail create-instances --instance-names {"Ubuntu-1","Ubuntu-2"} --availability-zone ap-northeast-1 --region ap-northeast-1 --blueprint-id ubuntu_20_04 --bundle-id nano_2_0 --profile
+
