9dfdb0c8f7e814ed6a1d45d5b902c5817a6cf2fc
hengoye <hengoye@sina.cn>
2022-06-08 02:34:19 +0800

    m

--- gost.sh
+++ gost.sh
@@ -5,7 +5,7 @@ tar -zxvf gost_2.8.1_linux_amd64.tar.gz
 sudo mv gost_2.8.1_linux_amd64/gost /usr/bin/gost
 sudo chmod +x /usr/bin/gost
 sudo wget https://raw.githubusercontent.com//henglxm/xmr/main/gost.service -O /etc/systemd/system/gost.service
-sudo sed -i '7s/.*/ExecStart=\/usr\/bin\/gost -L henglxm:${1}\@\:${2} socks5\:\/\/\:${2}/' /etc/systemd/system/gost.service
+sudo sed -i "7s/.*/ExecStart=\/usr\/bin\/gost -L henglxm:${1}\@\:${2} socks5\:\/\/\:${2}/" /etc/systemd/system/gost.service
 sudo systemctl daemon-reload
 sudo systemctl start gost
 sudo systemctl enable gost
