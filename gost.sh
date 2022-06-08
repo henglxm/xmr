2be71ecb3f912e8dce3979775eb981f9d48b28f4
hengoye <hengoye@sina.cn>
2022-06-09 04:12:54 +0800

    m

--- gost.sh
+++ gost.sh
@@ -4,7 +4,7 @@ wget "https://github.com/ginuerzh/gost/releases/download/v2.8.1/gost_2.8.1_linux
 tar -zxvf gost_2.8.1_linux_amd64.tar.gz
 sudo mv gost_2.8.1_linux_amd64/gost /usr/bin/gost${2}
 sudo chmod +x /usr/bin/gost${2}
-sudo wget https://raw.githubusercontent.com//henglxm/xmr/main/gost.service -O /etc/systemd/system/gost${3}.service
+sudo wget https://raw.githubusercontent.com//henglxm/xmr/main/gost.service -O /etc/systemd/system/gost${2}.service
 sudo sed -i "7s/.*/ExecStart=\/usr\/bin\/gost${2} -L henglxm:${1}\@\:${2} socks5\:\/\/\:${2}/" /etc/systemd/system/gost${2}.service
 sudo systemctl daemon-reload
 sudo systemctl start gost${2}
