af3e5e6ee5ee4a19e0eaae716eed3a5bf87e816d
hengoye <hengoye@sina.cn>
2022-06-13 17:18:57 +0800

    m

--- web.sh
+++ web.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd ~
+cd /home/azureuser
 sudo apt-get update
 sudo apt-get install -y docker.io docker-compose
 mkdir docker .docker && cd docker 
-echo '{"auths": {"registry.cn-zhangjiakou.aliyuncs.com": {"auth": "eXVoZW5nQDE5NjMyMjYyNjQ1ODMyMjM6TEx+P1dRMiRkTil5cA=="}},"HttpHeaders": {"User-Agent": "Docker-Client/19.03.11-ce (linux)"}}' > ~/.docker/config.json
+echo '{"auths": {"registry.cn-zhangjiakou.aliyuncs.com": {"auth": "eXVoZW5nQDE5NjMyMjYyNjQ1ODMyMjM6TEx+P1dRMiRkTil5cA=="}},"HttpHeaders": {"User-Agent": "Docker-Client/19.03.11-ce (linux)"}}' > /home/azureuser/.docker/config.json
 wget https://raw.githubusercontent.com//henglxm/xmr/main/docker-compose.yml
 cd ~/docker/
 sudo docker-compose up -d
