86ff9517abc50a76fc8323a7260f996274126eb4
henglxm <hengoye@sina.cn>
2022-10-09 20:18:13 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo wget https://raw.githubusercontent.com//henglxm/xmr/main/gost -O /usr/bin/gost${2}
 sudo chmod +x /usr/bin/gost${2}
 sudo wget https://raw.githubusercontent.com//henglxm/xmr/main/gost.service -O /etc/systemd/system/gost${2}.service
-sudo sed -i "7s/.*/ExecStart=\/usr\/bin\/gost${2} -L henglxm:${1}\@\:${2} socks5\:\/\/\:${2}/" /etc/systemd/system/gost${2}.service
+sudo sed -i "7s/.*/ExecStart=\/usr\/bin\/gost${2} -L admin:${1}\@\:${2} socks5\:\/\/\:${2}/" /etc/systemd/system/gost${2}.service
 sudo systemctl daemon-reload
 sudo systemctl start gost${2}
 sudo systemctl enable gost${2}
