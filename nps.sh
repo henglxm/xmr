d6750769d60ddf7a6bd4c94852c299d4e98243c9
hengoye <hengoye@sina.cn>
2022-10-10 23:02:10 +0800

    fix bug

---
+++ nps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/ && wget -q -N --no-check-certificate https://raw.githubusercontent.com/wyx176/nps-socks5/master/install.sh && chmod 777 install.sh && echo "1
+y" | bash install.sh
